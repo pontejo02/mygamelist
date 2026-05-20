0929b9747934f0ea0fba3f6cac95b795b525be3b
Luken Alava <pontejo02@gmail.com>
2026-05-20 12:00:00 +0200

    Añadir config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+// config.php — no subir a GitHub
+define('RAWG_KEY',     'ac5f8090d6c34000a20380f10e7b30ca');
+define('RAWG_URL',     'https://api.rawg.io/api');
+define('DB_HOST',      'localhost');
+define('DB_USER',      'root');
+define('DB_PASS',      '');
+define('DB_NAME',      'mygamelist');
+define('SITE_NAME',    'MyGameList');
+?>
